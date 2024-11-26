7477b7da9aeed4e3d83c62d56cc467ebea10cfb9
Simon Jefferies <simon.jefferies@inseinc.com>
2024-11-26 10:36:05 +0000

    Add SetsRequiredMembersAttribute class
    - Added `SetsRequiredMembersAttribute.cs` defining a new attribute class `SetsRequiredMembersAttribute` in the `System.Diagnostics.CodeAnalysis` namespace.
    - Updated `MissingAttributes.csproj` to include the new file for the `net6.0` target framework.
    - The attribute is designed for constructors, marked as `sealed`, and cannot be used multiple times on the same constructor or inherited by derived classes.

---
+++ MissingAttributes/SetsRequiredMembersAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace System.Diagnostics.CodeAnalysis;
+
+[System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+public sealed class SetsRequiredMembersAttribute : Attribute
+{
+}
